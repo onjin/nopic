02d8d8a537c50e015c11ebab84ff1c6647b12662
Marek Wywiał <onjinx@gmail.com>
2014-03-03 09:23:09 +0100

    Added homepage with generator

--- nopic.py
+++ nopic.py
@@ -2,9 +2,9 @@ from StringIO import StringIO
 
 from PIL import Image, ImageFont, ImageDraw
 import flask
-from flask import Flask, request
+from flask import Flask, request, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/media')
 
 
 def get_image_response(width, height, background='cccccc',
@@ -13,8 +13,15 @@ def get_image_response(width, height, background='cccccc',
     buf = StringIO()
     img = Image.new("RGB", (width, height), '#' + background)
 
+    # font
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("fonts/Lobster_1.3.otf", size)
+    fontname = font + '.otf'
+    try:
+        font = ImageFont.truetype("fonts/" + fontname, size)
+    except IOError:
+        fontname = 'lobster.otf'
+        font = ImageFont.truetype("fonts/" + fontname, size)
+
     w, h = font.getsize(text)
     draw.text(
         ((width - w) / 2, (height - h) / 2),
@@ -28,6 +35,11 @@ def get_image_response(width, height, background='cccccc',
     return resp
 
 
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
 @app.route('/<int:width>/',)
 @app.route('/<int:width>/<string:background>/',)
 @app.route('/<int:width>/<string:background>/<string:foreground>/',)
@@ -35,14 +47,17 @@ def get_image_response(width, height, background='cccccc',
 @app.route('/<int:width>x<int:height>/<string:background>/')
 @app.route('/<int:width>x<int:height>/<string:background>/<string:foreground>/')
 def fakeimg(width, height=None, background='cccccc', foreground='666666',
-            background_alpha=0, foreground_alpha=0, font=None):
+            background_alpha=0, foreground_alpha=0):
     if not height:
         height = width
     text = request.args.get('text', "%s x %s" % (width, height)).encode('utf8')
     size = int(request.args.get('size', 16))
+    font = request.args.get('font', 'lobster')
 
     return get_image_response(
         width, height, background, foreground, background_alpha,
         foreground_alpha, text, font, size
     )
 
+if __name__ == '__main__':
+    app.run(debug=True)
